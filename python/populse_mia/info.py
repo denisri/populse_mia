370e7fddf09c87ee3b5e37ebe5027ea1c9feca8f
Eric Condamine <37933899+servoz@users.noreply.github.com>
2019-04-29 13:17:31 +0200

    Update info.py

--- python/populse_mia/info.py
+++ python/populse_mia/info.py
@@ -60,7 +60,7 @@ if sys.version_info < (3 , 6) and sys.version_info >= (3 , 5):
         'pyyaml',
         'scikit-image',
         'scipy',
-        'SIP==4.19.3',  
+        'SIP==4.19.2',  
         'sqlalchemy',
         'snakeviz',
         'soma_workflow',
