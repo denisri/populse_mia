32959f8a84d5d2973b135dbd08fdeaa05e76cf0e
M40V <j.mammeri@gmail.com>
2019-05-02 15:59:45 +0200

    Docstring updates

--- python/populse_mia/pipeline_manager/iteration_table.py
+++ python/populse_mia/pipeline_manager/iteration_table.py
@@ -196,7 +196,6 @@ class IterationTable(QWidget):
 
         Used only for tests.
         """
-        print("called")
         push_button = self.push_buttons[-1]
         push_button.deleteLater()
         push_button = None
